6c65ecd38cd91cc6998e972bcb8a0fc6c0acdb53
Víctor Rivas <victor.rivas.sistemas@gmail.com>
2019-10-22 11:20:44 -0400

    Reducer: books amount

--- class002.js
+++ class002.js
@@ -1,28 +1,34 @@
 var victor = {
   nombre: 'Victor',
-  altura: 1.68
+  altura: 1.68,
+  libros: 5
 }
 
 var jordan = {
   nombre: 'Joran',
-  altura: 1.95
+  altura: 1.95,
+  libros: 20
 }
 
 var maria = {
   nombre: 'Maria',
-  altura: 1.86
+  altura: 1.86,
+  libros: 2
 }
 
 var paty = {
   nombre: 'Patricia',
-  altura: 1.57
+  altura: 1.57,
+  libros: 7
 }
 
-var personas = [victor, jordan, maria]
+var personas = [victor, jordan, maria, paty]
 
 const esAlta = ({altura}) => altura >= 1.85
 const toCms = persona => ({...persona, altura: persona.altura * 100})
+const cantidad = (acum, {libros}) => acum += libros
 
 console.log('Personas:', personas)
 console.log('Personas altas:', personas.filter(esAlta))
 console.log('Centimetros:', personas.map(toCms))
+console.log('Cantidad de libros:', personas.reduce(cantidad, 0))
