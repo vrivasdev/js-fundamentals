532e1be0bbbc386ba7d45db9dddca0860a838c77
Víctor Rivas <victor.rivas.sistemas@gmail.com>
2019-12-17 17:48:19 -0400

    Colour verification #1

--- game01.js
+++ game01.js
@@ -99,6 +99,7 @@
       const naranja = document.getElementById('naranja')
       const verde = document.getElementById('verde')
       const btnEmpezar = document.getElementById('btnEmpezar')
+      const ULTIMO_NIVEL = 10
 
       class Juego {
         constructor() {
@@ -120,10 +121,12 @@
         }
 
         generarSecuencia() {
-          this.secuencia = new Array(10).fill(0).map(n => Math.floor(Math.random() * 4))
+          this.secuencia = new Array(ULTIMO_NIVEL).fill(0).map(n => Math.floor(Math.random() * 4))
         }
 
         siguienteNivel() {
+          this.subnivel = 0
+          this.nombreAtributo = 'valor'
           this.iluminarSecuencia()
           this.agregarEventosClick()
         }
@@ -138,6 +141,19 @@
               return 'naranja'
             case 3:
               return 'verde'
+          }
+        }
+
+        transformarColorANumero(color) {
+          switch (color) {
+            case 'celeste':
+              return 0
+            case 'violeta':
+              return 1
+            case 'naranja':
+              return 2
+            case 'verde':
+              return 3
           }
         }
 
@@ -164,8 +180,31 @@
           this.colores.naranja.addEventListener('click', this.elegirColor)
         }
 
+        eliminarEventosClick() {
+          this.colores.celeste.removeEventListener('click', this.elegirColor)
+          this.colores.verde.removeEventListener('click', this.elegirColor)
+          this.colores.violeta.removeEventListener('click', this.elegirColor)
+          this.colores.naranja.removeEventListener('click', this.elegirColor)
+        }
+
         elegirColor(ev) {
-          console.log(this)
+          const nombreColor = ev.target.dataset.color
+          const numeroColor = this.transformarColorANumero(nombreColor)
+          this.iluminarColor(nombreColor)
+          if (numeroColor === this.secuencia[this.subnivel]) { // el color elegido es correcto
+            this.subnivel++
+            if (this.subnivel === this.nivel) { // usuario pasa de nivel
+              this.nivel++
+              this.eliminarEventosClick()
+              if (this.nivel === (ULTIMO_NIVEL + 1)) {
+                // Gano
+              } else {
+                setTimeout(this.siguienteNivel, 2000)
+              }
+            }
+          } else {
+
+          }
         }
       }
 
